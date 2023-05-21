06776092895c017c3aaeeb313e4f68f2ea5d2ca0
sagarstormborn <69786319+sagarstormborn@users.noreply.github.com>
2023-05-22 01:22:10 +0530

    Add files via upload
    minor update for training

--- model.py
+++ model.py
@@ -3,68 +3,66 @@ import torch.nn as nn
 import statistics
 import torchvision.models as models
 
-class EncoderCnn(nn.Module):
-    def __init__(self, embed_size,train_CNN=False,Inferance=False):
-        super(EncoderCnn,self).__init__()
-        self.Inferance=Inferance
-        self.train_CNN=train_CNN
-        self.inception=models.inception_v3(pretrained=True,aux_logits=False)
-        self.inception.fc=nn.Linear(self.inception.fc.in_features,embed_size)
-        self.relu =nn.ReLU()
-        self.times=[]
-        self.dropout=nn.Dropout(0.5)
+
+class EncoderCNN(nn.Module):
+    def __init__(self, embed_size, train_CNN=False):
+        super(EncoderCNN, self).__init__()
+        self.train_CNN = train_CNN
+        self.inception = models.inception_v3(pretrained=True)
+        self.inception.fc = nn.Linear(self.inception.fc.in_features, embed_size)
+        self.relu = nn.ReLU()
+        self.times = []
+        self.dropout = nn.Dropout(0.5)
 
     def forward(self, images):
         features = self.inception(images)
-        features = self.relu(features)
-        if self.Inferance:
-            self.dropout.eval()
-        features = self.dropout(features)
-
+        features = features.logits
+        features = self.dropout(self.relu(features))
         return features
 
-class DecoderRNN(nn.Module):
-    def __init__(self,embed_size,hidden_size,vocab_size,num_layers,Inferance=False):
-        super(DecoderRNN,self).__init__()
-        self.Inferance = Inferance
-        self.embed=nn.Embedding(vocab_size,embed_size)
-        self.lstm=nn.LSTM(embed_size,hidden_size,num_layers)
-        self.liner =nn.Linear(hidden_size,vocab_size)
-        self.dropout=nn.Dropout(0.5)
 
-    def forward(self,features ,captions):
-        if self.Inferance:
-            self.dropout.eval()
-        embedding=self.dropout(self.embed(captions))
-        embedding=torch.cat((features.unsqueeze(0),embedding),dim=0)
-        hiddens , _ =self.lstm(embedding) # using forced teacher model
-        outputs=self.liner(hiddens)
+class DecoderRNN(nn.Module):
+    def __init__(self, embed_size, hidden_size, vocab_size, num_layers):
+        super(DecoderRNN, self).__init__()
+        self.embed = nn.Embedding(vocab_size, embed_size)
+        self.lstm = nn.LSTM(embed_size, hidden_size, num_layers)
+        self.linear = nn.Linear(hidden_size, vocab_size)
+        self.dropout = nn.Dropout(0.5)
+
+    def forward(self, features, captions):
+        embeddings = self.dropout(self.embed(captions))
+        embeddings = torch.cat((features.unsqueeze(0), embeddings), dim=0)
+        hiddens, _ = self.lstm(embeddings)
+        outputs = self.linear(hiddens)
         return outputs
+
 
 class CNNtoRNN(nn.Module):
-    def __init__(self,embed_size,hidden_size, vocab_size,num_layers):
-        super(CNNtoRNN,self).__init__()
-        self.endcoderCNN=EncoderCnn(embed_size)
-        self.decoderRNN=DecoderRNN(embed_size,hidden_size,num_layers)
+    def __init__(self, embed_size, hidden_size, vocab_size, num_layers):
+        super(CNNtoRNN, self).__init__()
+        self.encoderCNN = EncoderCNN(embed_size)
+        self.decoderRNN = DecoderRNN(embed_size, hidden_size, vocab_size, num_layers)
 
-    def forward(self,image,caption):
-        features=self.endcoderCNN(image)
-        outputs=self.decoderRNN(features,caption)
+    def forward(self, images, captions):
+        features = self.encoderCNN(images)
+        outputs = self.decoderRNN(features, captions)
         return outputs
-    def caption_image(self,image,vocabulary,max_length=50):
-        result_caption=[]
+
+    def caption_image(self, image, vocabulary, max_length=50):
+        result_caption = []
+
         with torch.no_grad():
-            x=self.endcoderCNN(image).unsqueeze(0)
-            states=None
+            x = self.encoderCNN(image).unsqueeze(0)
+            states = None
 
             for _ in range(max_length):
-                hidden, states=self.decoderRNN.lstm(x,states)
-                output =self.decoderRNN.liner(hidden.squeeze(0))
-                predicted =output.argmax(1)
-                result_caption.append(predicted)
-                x=self.decoderRNN.embed(predicted.unsqueeze(0))
+                hiddens, states = self.decoderRNN.lstm(x, states)
+                output = self.decoderRNN.linear(hiddens.squeeze(0))
+                predicted = output.argmax(1)
+                result_caption.append(predicted.item())
+                x = self.decoderRNN.embed(predicted).unsqueeze(0)
 
-                if vocabulary.itos[predicted.item()]== "<EOS>":
+                if vocabulary.itos[predicted.item()] == "<EOS>":
                     break
-        return [vocabulary.itos[idx] for idx in result_caption]
 
+        return [vocabulary.itos[idx] for idx in result_caption]
